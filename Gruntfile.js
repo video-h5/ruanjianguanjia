049bdef90c60aa4eec1ea4259670ad4d7da30886
于涛 <121595113@qq.com>
2016-04-18 18:33:50 +0800

    优化ie兼容

--- Gruntfile.js
+++ Gruntfile.js
@@ -186,7 +186,7 @@ module.exports = function(grunt) {
                         '<%= config.app %>/images/{,*/}*',
                         '<%= config.app %>/**/*.js'
                     ],
-                    port: 9000,
+                    port: 8000,
                     server: {
                         baseDir: ['<%= config.app %>'],
                         index:"zhuanti-details.html"
